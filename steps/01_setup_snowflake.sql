1c1e43f487bac2d93052ca9d53789d5ac100088e
chandrapratapsingh007 <csingh@cloudbolt.io>
2025-02-10 23:18:50 -0500

    Update Git repository name in setup script

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -19,7 +19,7 @@ CREATE OR REPLACE api integration git_api_integration_demo
 
 
 -- Git repository object is similar to external stage
-CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.snowflake_devops
+CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.snowflake_devops_demo
   API_INTEGRATION = git_api_integration_demo
   ORIGIN = 'https://github.com/chandrapratapsingh08/snowflake_devops'; -- INSERT URL OF FORKED REPO HERE
 
