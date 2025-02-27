b25d7aa1fb02ff55468ac8d81453924e0a9b255c
chandrapratapsingh007 <csingh@cloudbolt.io>
2025-02-26 23:57:06 -0500

    Update dependencies and generate new RSA key pair

--- scripts/generate_keypair.sh
+++ scripts/generate_keypair.sh
@@ -1,5 +1,10 @@
 # Generate a private key
-openssl genpkey -algorithm RSA -out snowflake_devops_rsa_key.p8 -pkeyopt rsa_keygen_bits:2048
+openssl genrsa 2048 | openssl pkcs8 -topk8 -v2 des3 -inform PEM -out rsa_key.p8
+
+# https://govcloud.keepersecurity.us/vault/#detail/Ve0DZJJZ6t3qcTHl-M5qZg
+
+# Generate Base64 encoded private key
+base64 rsa_key.p8 > rsa_key.p8.b64
 
 # Extract the public key from the private key
-openssl rsa -in snowflake_devops_rsa_key.p8 -pubout -out snowflake_devops_rsa_key.pub
+openssl rsa -in rsa_key.p8 -pubout -out rsa_key.pub
